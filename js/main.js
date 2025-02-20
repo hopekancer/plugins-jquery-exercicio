76a8e44c0a1715356308eaca95ae3f7f27d2bd85
Hugo Casarisi <66367598+hopekancer@users.noreply.github.com>
2025-02-20 10:40:16 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,42 +1,52 @@
-$('#telefone').mascara('(00) 00000-0000', {
-    placeholder: '(DDD) 12345-6789'
+$(document).ready(function () {
+
+    $('#carousel-images').slick({
+        autoplay: true,
+        arrows: false
+    })
+
+
+    $('#telefone').mask('(00) 0 0000-0000', {
+        placeholder: '(DDD) 1 2345-6789'
+    })
+    
+    $('#cpf').mask('000.000.000-00', {
+        placeholder: '123.456.789-00'
+    })
+    
+    $('#cep').mask('00.000-000', {
+        placeholder: '01.2345-678'
+    })
+    
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+        },
+        submitHandler: function (form) {
+            alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
+            form.reset();
+        },
+        invalidHandler: function (form, validator) {
+            alert("Por favor, preencha os campos para prosseguir com a compra!");
+        }
+    })
 })
 
-$('#cpf').mascara('000.000.000-00', {
-    placeholder: '123.456.789-00'
-})
-
-$('#cep').mascara('00000-000', {
-    placeholder: '012345-678'
-})
-
-$('form').validate({
-    rules: {
-        nome: {
-            requerido: true
-        },
-        email: {
-            requerido: true,
-            email: true
-        },
-        telefone: {
-            requerido: true
-        },
-        endereco: {
-            requerido: true
-        },
-        cep: {
-            requerido: true
-        },
-        cpf: {
-            requerido: true
-        },
-    },
-    submitHandler: function (form) {
-        alert("Sua requisição foi enviada para análise, parabéns pela aquisição!");
-        form.reset();
-    },
-    invalidHandler: function (form, validator) {
-        alert("Por favor, preencha os campos para prosseguir com a compra!");
-    }
-})
